04c914c891567532c11b5e9ae2c9ce6718b1ab07
Anechka78 <7winx@bk.ru>
2023-03-06 21:36:01 +0200

    Creating an object generator with JSON data using Faker

--- frontend/controllers/DataController.php
+++ frontend/controllers/DataController.php
@@ -8,6 +8,10 @@ use common\models\Data;
 use frontend\models\Token;
 use common\models\User;
 use frontend\models\DataForm;
+use Faker\Factory;
+use yii\helpers\Json;
+use yii\web\Response;
+use stdClass;
 
 class DataController extends Controller
 {
@@ -25,5 +29,59 @@ class DataController extends Controller
         ]);        
         
     }
+	
+	/**
+     * Create random JSON obj
+     *
+     * @return array
+     */
+	public function actionCreateData()
+    {
+		Yii::$app->response->format = Response::FORMAT_JSON;
+		
+        $faker = Factory::create();   
+		$res = [];
+		// generate the object with nested arrays
+		$object = [
+			'name' => $faker->name,
+			'email' => $faker->email,
+			'age' => $faker->numberBetween(18, 99),
+			'address' => [
+				'street' => $faker->streetAddress,
+				'city' => $faker->city,
+				'state' => $faker->state,
+				'zip' => $faker->postcode,
+				'phone' => $faker->phoneNumber,
+				'nested_array' => self::generateRandomNestedArray($faker),
+			],
+		];
+		
+		// convert the object to JSON
+		$json = Json::encode($object);
+		
+		$res['data'] = $json;
+		
+        return $res;
+    }
+	
+	public static function generateRandomNestedArray($faker, $depth = 0)
+	{
+		$result = new stdClass(); // создаем новый пустой объект stdClass
+
+		// generate between 1 and 5 elements for the array
+		$numElements = $faker->numberBetween(1, 5);
+
+		for ($i = 0; $i < $numElements; $i++) {
+			if ($depth < 2) {
+				// generate a nested object with a maximum depth of 2
+				$result->{$faker->word} = self::generateRandomNestedArray($faker, $depth + 1);
+			} else {
+				// generate a random value for the object property
+				$result->{$faker->word} = $faker->randomElement([$faker->word, $faker->numberBetween(1, 100)]);
+			}
+		}
+
+		return $result;
+	}
 
 }
--- frontend/views/data/savedata.php
+++ frontend/views/data/savedata.php
@@ -6,27 +6,54 @@
 
 use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\web\View;
 
 $this->title = 'Add data';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
     <h1><?= Html::encode($this->title) ?></h1>
-<p>Please fill out the following fields to save data:</p>
+	<p>Please fill out the following fields to save data:</p>
 
-    <div class="row">
-        <div class="col-lg-5">
+	<div class="row">
+		<div class="col-lg-6">
 
-<?php $form = ActiveForm::begin([
-	'action' => '/data/send-data'
-	]) ?>
-    <?= $form->field($model, 'type_request')->radioList(['GET' => 'GET', 'POST' => 'POST']) ?>
-    <?= $form->field($model, 'token')->textInput(['placeholder' => 'Enter authorization key']) ?>
-    <?= $form->field($model, 'data')->textarea(['rows' => 6])->label(Html::tag("span", 'Enter the data')) ?>
-    <div class="form-group">
-        <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
+	<?php $form = ActiveForm::begin([
+		'action' => '/data/send-data'
+		]) ?>
+		<?= $form->field($model, 'type_request')->radioList(['GET' => 'GET', 'POST' => 'POST']) ?>
+		<?= $form->field($model, 'token')->textInput(['placeholder' => 'Enter authorization key']) ?>
+		<?= $form->field($model, 'data')
+		->textarea(['rows' => 6, "id" => "data-textarea"])
+		->label(Html::tag("span", 'Enter the data manually', ["id" => "save-data-span"]) . ' ' . Html::a('Create a random JSON object', '#', ['class' => 'btn btn-primary', 'id' => 'create-fake-data-btn', 'style' => 'font-weight: bold']), ['id' => 'save-data-label']); ?>
+		<div class="form-group">
+			<?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
+		</div>
+	<?php ActiveForm::end() ?>
+		</div>
     </div>
-<?php ActiveForm::end() ?>
 </div>
-    </div>
-</div>
+
+<?php
+$js = <<<HERE
+$(function() {
+    $(document).on("click", "#create-fake-data-btn", function (e) {		
+        e.preventDefault(); 
+                
+        $.ajax({
+            url: '/data/create-data',
+            type: 'POST',
+            cache: false,
+            dataType: 'json',            
+            success: function(res) {
+				if(typeof(res.data) != 'undefined') {
+					$('#data-textarea').text(res.data);
+				}
+            }
+        });
+    });
+});
+HERE;
+
+$this->registerJs($js, View::POS_END);
+?>
